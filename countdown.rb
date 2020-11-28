70a112d1b7af7113d2a72b50d3a30a9f2c89a294
Diana Utan <didiutzi@gmail.com>
2020-11-28 16:51:38 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,15 +4,20 @@ def countdown(num)
   while num  > 0 
     puts "#{num} SECOND(S)!"
     num -=1
-end
+  end
+puts "HAPPY NEW YEAR!"
  return "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(num)
   while num > 0 
-  puts "#{num} SECOND(S)!"
-  sleep 1
-  num -=1
-end
+    puts "#{num} SECOND(S)!"
+    sleep 1
+    num -=1
+  end
+  puts "HAPPY NEW YEAR!"
   return "HAPPY NEW YEAR!"
 end
+
+# countdown_with_sleep(3)
+# countdown(3)
